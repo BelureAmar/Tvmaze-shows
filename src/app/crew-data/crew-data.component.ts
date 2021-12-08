a85b4aa1f7366185cf679eaf7e13da59aee601ac
Amar <95026468+BelureAmar@users.noreply.github.com>
2021-12-08 14:59:42 +0530

    Removed unwanted functions

--- src/app/crew-data/crew-data.component.ts
+++ src/app/crew-data/crew-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ShowModel } from '../Model/models';
 
 @Component({
@@ -6,14 +6,8 @@ import { ShowModel } from '../Model/models';
   templateUrl: './crew-data.component.html',
   styleUrls: ['./crew-data.component.css']
 })
-export class CrewDataComponent implements OnInit {
+export class CrewDataComponent {
 
   @Input() crewList: any[];
   @Input() name: string;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }

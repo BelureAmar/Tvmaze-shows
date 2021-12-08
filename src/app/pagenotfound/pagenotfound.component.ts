89e3ce8cb6418811e2193a12586766d3438cc8fe
Amar <95026468+BelureAmar@users.noreply.github.com>
2021-12-08 15:03:42 +0530

    Removed unwanted functions

--- src/app/pagenotfound/pagenotfound.component.ts
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pagenotfound',
   templateUrl: './pagenotfound.component.html',
   styleUrls: ['./pagenotfound.component.css']
 })
-export class PageNotFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class PageNotFoundComponent {
 
 }
